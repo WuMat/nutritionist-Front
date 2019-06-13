02731190632f4d2a18c834105c7d79902ca7b66b
WuMat <wuzikm@gmail.com>
2019-06-13 23:12:02 +0200

    url heroku

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.URL || "https://lifestyles-back.herokuapp.com/"
+  baseURL: process.env.URL || "https://tranquil-depths-70068.herokuapp.com/"
 });
 
 export default instance;
