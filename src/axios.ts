cd598a02feb07cd18ecd9c712608b2a16abc38b7
WuMat <wuzikm@gmail.com>
2019-08-05 22:50:41 +0200

    asd

--- src/axios.ts
+++ src/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.URL_BASE || "http://localhost:8080"
-  // baseURL: process.env.URL_BASE || "http://116.203.197.143"
+  // baseURL: process.env.URL_BASE || "http://localhost:8080"
+  baseURL: process.env.URL_BASE || "http://116.203.197.143"
 });
 
 export default instance;
