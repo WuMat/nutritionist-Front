ba3136ff04977a9b7a409f1edab4ff6eef9f64b9
WuMat <wuzikm@gmail.com>
2019-08-05 22:37:18 +0200

    asd

--- src/utils/URL.ts
+++ src/utils/URL.ts
@@ -1,2 +1,2 @@
-export const URL = process.env.URL || "http://116.203.197.143/img/";
-// export const URL = process.env.URL || "http://localhost:8080/img/";
+// export const URL = process.env.URL || "http://116.203.197.143/img/";
+export const URL = process.env.URL || "http://localhost:8080/img/";
